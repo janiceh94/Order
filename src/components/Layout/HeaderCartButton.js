020ce12cb312d365452a5fad4785d072e1bac297
janiceh94 <haesung94@gmail.com>
2023-04-05 15:22:52 -0700

    added filler code

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,11 +1,18 @@
 import styles from "./HeaderCartButton.module.css";
+import CartIcon from "../Cart/CartIcon";
 
 export default function HeaderCartButton(props){
     return (
         <button>
-            <span></span>
-            <span></span>
-            <span></span>
+            <span>
+                <CartIcon/>
+            </span>
+            <span>
+                Your Cart
+            </span>
+            <span>
+                Item Count
+            </span>
         </button>
     )
 }
