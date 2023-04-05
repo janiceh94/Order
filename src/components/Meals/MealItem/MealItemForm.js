1b34b401736e185f6d6a106e05a01e529d8cee8a
janiceh94 <haesung94@gmail.com>
2023-04-05 16:55:38 -0700

    pasted mealItemForm css and layout

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,7 +1,10 @@
 import styles from "./MealItemForm.module.css";
 
-export default function MealItemForm(){
+export default function MealItemForm(props){
     return (
-        
+        <form className={styles.form}>
+            <input/>
+            <button>+ Add</button>
+        </form>
     )
 }
