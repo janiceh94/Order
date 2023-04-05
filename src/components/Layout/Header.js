552a71478fae28d5205c67649f83a5b571be704e
janiceh94 <haesung94@gmail.com>
2023-04-05 15:24:49 -0700

    replaced button in header with CartIcon import

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import styles from "./Header.module.css";
 import { Fragment } from "react";
 // Allows you to return multiple elements from a React component by allowing you to group a list of children without adding extra nodes to the DOM
 import mealsImage from "../../assets/meals.jpeg";
+import CartIcon from "../Cart/CartIcon";
 
 export default function Header(props){
     return (
         <Fragment>
             <header className={styles.header}>
                 <h1>Food Orders</h1>
-                <button>Cart</button>
+                <CartIcon/>
             </header>
             <div className={styles[`main-image`]}>
                 <img src={mealsImage} alt="Table filled with food"/>
