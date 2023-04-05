7100bbe734dab21da70ed1544fbdf2d150a1ae5f
janiceh94 <haesung94@gmail.com>
2023-04-05 15:23:53 -0700

    connected css

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -3,14 +3,14 @@ import CartIcon from "../Cart/CartIcon";
 
 export default function HeaderCartButton(props){
     return (
-        <button>
-            <span>
+        <button className={styles.button}>
+            <span className={styles.icon}>
                 <CartIcon/>
             </span>
             <span>
                 Your Cart
             </span>
-            <span>
+            <span className={styles.badge}>
                 Item Count
             </span>
         </button>
