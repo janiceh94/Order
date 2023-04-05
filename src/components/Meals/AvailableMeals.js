1bd680b4275119dcae2c44b8f9bce5e49e49cded
janiceh94 <haesung94@gmail.com>
2023-04-05 16:52:16 -0700

    imported MealITem to AvailableMeals and added prop names

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import styles from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
     {
@@ -30,7 +31,12 @@ const DUMMY_MEALS = [
 
 export default function AvailableMeals(){
     const mealsList = DUMMY_MEALS.map(meal => 
-            <li>{meal.name}</li>
+            <MealItem 
+                key={meal.id}
+                name={meal.name}
+                description={meal.description}
+                price={meal.price}
+            />
         );
 
     return (
