96dad7cdc76983fce153227d217d8f64274a5784
janiceh94 <haesung94@gmail.com>
2023-04-05 19:22:25 -0700

    adding modal via react portal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,7 +1,31 @@
+//added to public-index.html-line 31
+// allows developers to render their elements outside the React hierarch tree without compromisong the parent-child relationship between components
 import styles from "./Modal.module.css";
+import { Fragment } from "react";
+import ReactDom from "react-dom";
+
+const Backdrop = (props) => {
+    return (
+        <div className={styles.backdrop}>
+        </div>
+    )
+}
+const ModalOverlay = (props) => {
+    return (
+        <div className={styles.modal}>
+            <div className={styles.content}>
+                {props.children}
+            </div>
+        </div>
+    )
+}
+const portalElement = document.getElementById("overlays");
 
 export default function Modal(props){
     return (
-        
+        <Fragment>
+            {ReactDom.createPortal(<Backdrop/>, portalElement)}
+            {ReactDom.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+        </Fragment>
     )
 }
