5dc33d931ec3e84dbf338e942481ffa388e359ac
janiceh94 <haesung94@gmail.com>
2023-04-05 22:14:16 -0700

    using useContext and react reduce

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -6,6 +6,15 @@ import CartContext from "../../store/cart-context";
 
 export default function HeaderCartButton(props){
     const cartContext = useContext(CartContext);
+    // .reduce executes a user-supplied "reducer" callback function on each element of the array, in order, passing in the return value from the calculation on the preceding element. Final result if running the reducer across all elements of the array is a single value
+    const numberOfCartItems = cartContext.items.reduce
+    // calls two arguments
+        // currentNumber - for every item in that array on what you're calling reduce
+        // item - item which it is currently having a look at
+    ((currentNumber, item) => {
+        return currentNumber + item.amount;
+        // 0 is starting value
+    }, 0);
 
     return (
         <button className={styles.button} onClick={props.onClick}>
@@ -16,7 +25,7 @@ export default function HeaderCartButton(props){
                 Your Cart
             </span>
             <span className={styles.badge}>
-                Item Count
+                {numberOfCartItems}
             </span>
         </button>
     )
