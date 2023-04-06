be9aa01ed15aac4acbcf0404ffb9585bfeeb8f3f
janiceh94 <haesung94@gmail.com>
2023-04-05 22:27:52 -0700

    logic for adding items to the cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,15 +1,42 @@
+// Accepts a reducer function and an initial state
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
-export default function CartProvider(props){
-    const handleAddItemToCart = item => {
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+};
+const cartReducer = (state, action) => {
+    if(action.type === "ADD"){
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
+    return defaultCartState;
+}
 
+export default function CartProvider(props){
+    // cartReducer - point at our reducer function
+    //defaultCartState - initial state
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+    const handleAddItemToCart = item => {
+        dispatchCartAction({
+            type: "ADD",
+            item: item
+        });
     };
     const handleRemoveItemFromCart = id => {
-
+        dispatchCartAction({
+            type: "REMOVE",
+            id: id
+        })
     };
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: handleAddItemToCart,
         removeItem: handleRemoveItemFromCart,
     }
