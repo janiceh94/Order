34026b1778c28b9ca94037c80e748f0f06941174
janiceh94 <haesung94@gmail.com>
2023-04-05 22:05:34 -0700

    header cart will re render when value changes

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,12 @@
+// Will always re-render when the context value changes
+import { useContext } from "react";
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
 export default function HeaderCartButton(props){
+    const cartContext = useContext(CartContext);
+
     return (
         <button className={styles.button} onClick={props.onClick}>
             <span className={styles.icon}>
