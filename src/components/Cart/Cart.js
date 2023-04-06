1630c44a922091f43115405d7422800933ae32d9
janiceh94 <haesung94@gmail.com>
2023-04-05 19:09:57 -0700

    cart layout

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,23 @@
 import styles from "./Card.module.css";
 
 export default function Cart(props){
-    return (
+    const cartItems = <ul className={styles[`cart-items`]}>{
+            [{id:"c1", name:"Sushi", amount: 2, price: 12.99}].map(item => 
+                <li>{item.name}</li>
+            )
+        }</ul>
 
+    return (
+        <div>
+            {cartItems}
+            <div className={styles.total}>
+                <span>Total Amount</span>
+                <span>Total</span>
+            </div>
+            <div className={styles.actions}>
+                <button className={styles[`button--alt`]}>Close</button>
+                <button className={styles.button}>Order</button>
+            </div>
+        </div>
     )
 }
